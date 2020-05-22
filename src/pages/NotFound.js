cbeafd15e229e02460770fababf018b85f921e43
Ninfa Niño <ninfa_sys@hotmail.com>
2020-05-21 22:29:28 -0500

    state project

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ButtonBack } from '../components/ButtonBack';
+
+const NotFound = () => (
+    <div>
+        <h1 className="title">404!</h1>
+        <h2 className="subtitle">Page not found</h2>
+        <ButtonBack/>
+    </div>
+)
+
+export default NotFound
